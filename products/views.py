34c966a90026a3d85788dde04f099d8c47c76adb
Dye <anthony.dye@ies-co.com>
2022-04-01 11:24:59 -0500

    Created New View Function for Getting Products by ID + Added New URLS Path for Products Detail View Function

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,6 +19,23 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status= status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404 (Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = 
+
+
+    print(pk)
+    return Response(pk)
+
+
+
+
 
 
 
